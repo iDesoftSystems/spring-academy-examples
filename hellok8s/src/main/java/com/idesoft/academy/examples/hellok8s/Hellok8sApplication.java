cc49e5800104831b4a39bf9fe4c586b93a351a80
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-01-05 22:17:29 +0100

    (hellok8s): initial hellok8s project

---
+++ hellok8s/src/main/java/com/idesoft/academy/examples/hellok8s/Hellok8sApplication.java
@@ -0,0 +1,13 @@
+package com.idesoft.academy.examples.hellok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellok8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellok8sApplication.class, args);
+	}
+
+}
