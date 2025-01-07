420ecb92c403d3709166e18f7a1ba6b1b9639e32
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-01-07 10:09:46 +0100

    (jetbrainsexposed): initial jetbrainsexposed project

---
+++ jetbrainsexposed/src/main/java/com/idesoft/academy/examples/jetbrainsexposed/JetbrainsexposedApplication.java
@@ -0,0 +1,13 @@
+package com.idesoft.academy.examples.jetbrainsexposed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetbrainsexposedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetbrainsexposedApplication.class, args);
+	}
+
+}
---
+++ jetbrainsexposed/src/test/java/com/idesoft/academy/examples/jetbrainsexposed/JetbrainsexposedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idesoft.academy.examples.jetbrainsexposed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetbrainsexposedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
